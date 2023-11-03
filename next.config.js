0676d10ac400e79806dbc9f92c09c0bd392027f0
Praveen Sansaniwal <indian.devopsguru@gmail.com>
2023-11-03 10:45:40 +0530

    Added conf

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
     ],
   },
   env: {
-    baseUrl: "https://my-property-go-backend.onrender.com/api",
+    baseUrl: "https://api.wonderplots.com/api",
   },
   typescript: {
     // Dangerously allow production builds to successfully complete even if
